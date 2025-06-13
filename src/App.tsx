86831622dda1f136de4caa069b67d9c92910ec7d
LankyMoose <rausten93@gmail.com>
2025-06-13 13:42:47 +1200

    remove title

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,20 @@ import { createMilestoneSet, jsonUtils, settingsOpen } from "./state"
 export function App() {
   return (
     <div className="flex flex-col gap-4 p-4 max-h-screen">
-      <div className="flex gap-4 justify-between items-center">
-        <h1 className="text-2xl font-bold">Milestone Tracker</h1>
-        <Row gap="gap-4">
-          <IconButton onclick={() => createMilestoneSet()}>
-            <CreateIcon width="1rem" />
-            Create New Set
-          </IconButton>
-          <IconButton onclick={() => jsonUtils.importSet()}>
-            <ImportIcon width="1rem" />
-            Import Set
-          </IconButton>
-          <IconButton onclick={() => (settingsOpen.value = true)}>
-            <CogIcon width="1rem" />
-            Settings
-          </IconButton>
-        </Row>
-      </div>
+      <Row gap="gap-4" className="justify-end">
+        <IconButton onclick={() => createMilestoneSet()}>
+          <CreateIcon width="1rem" />
+          Create New Set
+        </IconButton>
+        <IconButton onclick={() => jsonUtils.importSet()}>
+          <ImportIcon width="1rem" />
+          Import Set
+        </IconButton>
+        <IconButton onclick={() => (settingsOpen.value = true)}>
+          <CogIcon width="1rem" />
+          Settings
+        </IconButton>
+      </Row>
       <MilestoneSets />
       <ActiveMilestoneSetView />
       <MilestoneSetEditor />
